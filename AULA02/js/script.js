3a029f1ac2a03d6bb02055464e8210c76b975d6b
tiago <tiago.duarte@portalsesisp.org.br>
2024-03-26 10:16:54 -0300

    alteracoes do dia 26/03

--- AULA02/js/script.js
+++ AULA02/js/script.js
@@ -1,3 +1,16 @@
 alerta = () => {
-    alert("Olá, eu sou um alerta!🚨")
-}
+  alert("Olá, eu sou um alerta!🚨");
+};
+const enviar = () => {
+  let nome = document.getElementById("nome").value;
+
+  document.getElementById(
+    "resposta"
+  ).innerHTML = `<h2>Olá ${nome}, formulario enviado com sucesso !</h2>`
+
+  if (nome == "") {
+    alert("Bem vindo!");
+  } else {
+    alert(`Olá ${nome}, bem vindo!`);
+  }
+};
